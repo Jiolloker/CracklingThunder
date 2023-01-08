b2c208766e5d63c9b590dddac6ffaf6cec1658d5
jiolloker <jiolloker@hotmail.com>
2023-01-07 22:34:55 -0300

    rev2

--- wp_server.sh
+++ wp_server.sh
@@ -16,7 +16,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 #Añadir repositorio de docker
 echo "Añadir repositorio de docker"
 sleep 2
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_releas while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Post e -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 echo "\n\r"
 
@@ -39,16 +39,16 @@ mkdir -p ~/wordpress/html
 #Configurar MariaDB
 echo "Configurar MariaDB"
 sleep 2
-read -r -p "Escribir Mysql root password para configurar la base de datos: " MYSQL_ROOT_pass
+read -r -p "Escribir Mysql root password para configurar la base de datos: " MYSQL_ROOT_PASS
 
-read -r -p "Escribir Mysql nombre de usuario: " MYSQL_user
+read -r -p "Escribir Mysql/wordpress nombre de usuario: " MYSQL_WP_USER
 
-read -r -p "Escribir contraseña para mysql: " MYSQL_PASS
+read -r -p "Escribir contraseña para mysql/wordpress: " MYSQL_WP_PASS
 echo "Se guardaran credenciales de mysql en mysqlcred.txt"
 sleep 3
 echo $MYSQL_ROOT_pass $MYSQL_user $MYSQL_PASS mysqlcred.txt
 
-sudo docker run -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_pass -e MYSQL_USER=$MYSQL_user -e MYSQL_PASSWORD=$MYSQL_PASS -e MYSQL_DATABASE=wpdb -v /root/wordpress/database:/var/lib/mysql --name wordpressdb -d mariadb
+sudo docker run -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASS -e MYSQL_USER=$MYSQL_WP_USER -e MYSQL_PASSWORD=$MYSQL_WP_PASS -e MYSQL_DATABASE=wpdb -v /root/wordpress/database:/var/lib/mysql --name wordpressdb -d mariadb
 
 
 
@@ -78,14 +78,8 @@ sudo docker pull wordpress:latest
 #Crear nuevo nombre de contenedor
 echo "Configuración de Wordpress"
 sleep 2
-read -r -p "Escribir nombre de usuario de Wordpress: " WP_DB_USER
 
-read -r -p "Escribir password de usuario de Wordpress: " WP_DB_PASS
-echo "Se guardaron las credenciales de Wordpress en wpdata.txt"
-sleep 3
-echo $WP_DB_USER $WP_DB_PASS wpdata.txt
-
-sudo docker run -e WORDPRESS_DB_USER=$WP_DB_USER -e WORDPRESS_DB_PASSWORD=$WP_DB_PASS -e WORDPRESS_DB_NAME=wpdb -p 8081:80 -v /root/wordpress/html:/var/www/html --link wordpressdb:mysql --name wpcontainer -d wordpress
+sudo docker run -e WORDPRESS_DB_USER=$MYSQL_WP_USER -e WORDPRESS_DB_PASSWORD=$MYSQL_WP_PASS -e WORDPRESS_DB_NAME=wpdb -p 8081:80 -v /root/wordpress/html:/var/www/html --link wordpressdb:mysql --name wpcontainer -d wordpress
 
 
 
